239ddc337a5dc6df67c6b4ec7bf88cbf538e37f3
argkan13 <argiriskanellos@gmail.com>
2023-08-25 19:02:39 +0300

    new 7:02

--- public/admin_ajax_req.js
+++ public/admin_ajax_req.js
@@ -1,109 +1,69 @@
 $(document).ready(function () {
-    let dataArray = []; // Empty array to store fetched data
-
-    aksiologhsh();
+    // Attach a click event handler to the "Discounts" button
+    $("#displayButton").click(function () {
+        // Call the function to fetch and display data items
+        aksiologhsh();
+    });
 
     function aksiologhsh() {
         $.ajax({
             type: "GET",
             url: "/users/map/aksiologhsh",
             success: function(result) {
-                dataArray = result;
+                let data = result;
+                const dataItems = [];
 
-                // After storing data in dataArray, call a function to display it on admin.ejs
-                displayDataOnPage();
+                for (let i = 0; i < result.length; i++) {
+                    let discount_on = data[i].discount_on;
 
+                    if (discount_on === 1) {
+                        let title = data[i].store_name;
+                        let loc = [data[i].store_latitude, data[i].store_longitude];
+                        let user_name = data[i].name;
+                        let product_name = data[i].name;
+                        let price = data[i].price;
+                        let date = data[i].date_entered;
+                        let product_id = data[i].product_id;
 
-function displayDataOnPage() {
-        const container = $('#dataContainer');
-        container.empty();
+                        const dataItem = {
+                            title,
+                            loc,
+                            user_name,
+                            product_name,
+                            price,
+                            date,
+                            product_id
+                        };
 
-       
-        for (let i = 0; i < result.length; i++) {
-            let discount_on = result[i].discount_on;
-    
-            if (discount_on === 1) {
-              let title = result[i].store_name;
-              let loc = result[i].store_latitude + ',' + result[i].store_longitude;
-              let user_name = result[i].name;
-              let product_name = result[i].name;
-              let price = result[i].price;
-              let date = result[i].date_entered;
-              let product_id = result[i].product_id;
-    
-              
-            }
-          }
+                        dataItems.push(dataItem);
+                    }
+                }
 
-
-
-
-
-
-
-    }
-
-    // Add a click event handler for the displayButton
-    $("#displayButton").click(function () {
-        aksiologhsh();
-    });
-
-    // Handle edit button click
-    $("#dataContainer").on("click", ".editButton", function () {
-        const marker = $(this).closest(".marker");
-        const dataIndex = marker.data("index");
-        const dataEntry = dataArray[dataIndex];
-
-        // Populate edit form with data
-        populateEditForm(dataEntry);
-        openEditModal(dataIndex);
-    });
-
-    // Handle save changes button click
-    $("#saveEditButton").click(function () {
-        const dataIndex = $("#editModal").data("index");
-        const editedData = collectEditedDataFromForm();
-        updateDataArray(dataIndex, editedData);
-        displayDataOnPage();
-        closeEditModal();
-    });
-
-    function populateEditForm(dataEntry) {
-        $("#editTitle").val(dataEntry.title);
-        $("#editLoc").val(dataEntry.loc);
-        $("#editUser").val(dataEntry.user_name);
-    }
-
-    function openEditModal(dataIndex) {
-        $("#editModal").data("index", dataIndex);
-        $("#editModal").show();
-    }
-
-    function collectEditedDataFromForm() {
-        const editedData = {
-            title: $("#editTitle").val(),
-            loc: $("#editLoc").val(),
-            user_name: $("#editUser").val(),
-        };
-        return editedData;
-    }
-
-    function updateDataArray(dataIndex, editedData) {
-        dataArray[dataIndex] = editedData;
-    }
-
-    function closeEditModal() {
-        $("#editModal").hide();
-    }
-
-
-
-
-
-
+                // Call a function to display the data items
+                displayDataItems(dataItems);
             }
         });
     }
 
-    
+    function displayDataItems(dataItems) {
+        // Clear previous data if any
+        $("#dataContainer").empty();
+
+        // Iterate through dataItems and generate HTML for each item
+        for (let i = 0; i < dataItems.length; i++) {
+            let dataItem = dataItems[i];
+            // Generate HTML for the data item using dataItem properties
+            let itemHTML = `<div class="discount-item">
+                <h3>${dataItem.title}</h3>
+                <p>User: ${dataItem.user_name}</p>
+                <p>Product: ${dataItem.product_name}</p>
+                <p>Price: ${dataItem.price}</p>
+                <p>Date: ${dataItem.date}</p>
+            </div>`;
+
+            // Append the HTML to the dataContainer
+            $("#dataContainer").append(itemHTML);
+        }
+    }
 });
+
