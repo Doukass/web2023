03501dd09250a30580a0160047e9f97ed37d334c
christos-pilavidis <chris.pilavidis@hotmail.com>
2023-08-26 00:58:12 +0300

    submit admin

---
+++ public/admin.js
@@ -0,0 +1,76 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const dropArea = document.getElementById("dropArea");
+    const jsonInput = document.getElementById("jsonInput");
+    const dataContainer = document.getElementById("dataContainer");
+    const uploadIcon = document.getElementById("uploadIcon");
+  
+    dropArea.addEventListener("dragover", function (e) {
+      e.preventDefault();
+      dropArea.classList.add("active");
+    });
+  
+    dropArea.addEventListener("dragleave", function () {
+      dropArea.classList.remove("active");
+    });
+  
+    dropArea.addEventListener("drop", function (e) {
+      e.preventDefault();
+      dropArea.classList.remove("active");
+  
+      const file = e.dataTransfer.files[0];
+      handleFile(file);
+    });
+  
+    dropArea.addEventListener("click", function () {
+      jsonInput.click();
+    });
+  
+    jsonInput.addEventListener("change", function () {
+      const file = jsonInput.files[0];
+      handleFile(file);
+    });
+  
+    function handleFile(file) {
+        if (file) {
+          const reader = new FileReader();
+          reader.onload = function (e) {
+            const fileContent = e.target.result; // Debugging: log this content
+            try {
+              const jsonData = JSON.parse(fileContent);
+              displayJsonData(jsonData);
+              showUploadIcon();
+            } catch (error) {
+              console.error("Error parsing JSON:", error);
+            }
+          };
+          reader.readAsText(file);
+        }
+      }
+    function displayJsonData(data) {
+      const marker = document.createElement("div");
+      marker.classList.add("marker");
+  
+      for (const key in data) {
+        if (data.hasOwnProperty(key)) {
+          const keyElement = document.createElement("h3");
+          keyElement.textContent = key;
+  
+          const valueElement = document.createElement("p");
+          valueElement.textContent = JSON.stringify(data[key]);
+  
+          marker.appendChild(keyElement);
+          marker.appendChild(valueElement);
+        }
+      }
+  
+      dataContainer.appendChild(marker);
+    }
+  
+    function showUploadIcon() {
+      uploadIcon.style.display = "block";
+      setTimeout(() => {
+        uploadIcon.style.display = "none";
+      }, 2000); // Show for 2 seconds
+    }
+  });
+  
